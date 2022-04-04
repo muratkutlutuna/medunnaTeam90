98e4cb629142a2b6e2cecef17cf53acf8b44191a
mehlika <mehlika.balaban@gmail.com>
2022-04-04 13:45:41 -0700

    MehlikaPage duzenlendi

--- src/test/java/pages/MehlikaPage.java
+++ src/test/java/pages/MehlikaPage.java
@@ -11,29 +11,68 @@ public class MehlikaPage extends BasePage{
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    //   @FindBy(id="twotabsearchtextbox")
-    //    public WebElement aramaKutusu;
-
-    //    @FindBy(xpath = "(//div[@class='a-section a-spacing-small a-spacing-top-small'])[1]")
-    //    public WebElement sonucYazisiElementi;
+    // ---- home page
     @FindBy (xpath = "(//img[@alt='Logo'])")
     public WebElement homePageLogo;
-
     @FindBy (xpath = "(//span[text()='Make an'])")
-    public WebElement MakeAnAppointmentButton;       //Home Page
+    public WebElement MakeAnAppointmentButton;
 
+    // ---- first name
     @FindBy(xpath = "(//label[text()='First Name']")
     public WebElement firstNameTag;
-
     @FindBy(xpath = "//input[@name='firstName']")
     public WebElement firstNameInputBox;
-
     @FindBy(xpath = "//div[text()='Your FirstName is required.']")
     public WebElement firstNameWarning;
 
+    // ---- last name
     @FindBy(xpath ="//label[text()='Last Name']" )
     public WebElement lastNameTag;
+    @FindBy(xpath = "//input[@name='lastName']")
+    public WebElement lastNameInputBox;
+    @FindBy(xpath ="//div[text()='Your LastName is required.']" )
+    public WebElement lastNameWarning;
 
-    @FindBy(xpath = "")
+    // ---- SSN
+    @FindBy(xpath ="//label[text()='SSN']" )
+    public WebElement ssnTag;
+    @FindBy(xpath = "//input[@name='ssn']")
+    public WebElement ssnInputBox;
+    @FindBy(xpath ="//div[text()='Your SSN is required.']" )
+    public WebElement ssnWarning;
+
+    // ---- email
+    @FindBy(xpath ="//label[text()='Email']" )
+    public WebElement emailTag;
+    @FindBy(xpath = "//input[@name='email']")
+    public WebElement emailInputBox;
+    @FindBy(xpath ="//div[text()='Your email is required.']" )
+    public WebElement emailWarning;
+
+    // ---- phone
+    @FindBy(xpath ="//label[text()='Phone']" )
+    public WebElement phoneTag;
+    @FindBy(xpath = "//input[@name='phone']")
+    public WebElement phoneInputBox;
+    @FindBy(xpath ="//div[text()='Phone number is required.']" )
+    public WebElement phoneWarning;
+
+    // ---- Appointment DateTime
+    @FindBy(xpath ="//span[text()='Appointment DateTime']" )
+    public WebElement dateTimeTag;
+    @FindBy(xpath = "//input[@name='appoDate']")
+    public WebElement dateTimeInputBox;
+    @FindBy(xpath ="//span[text()='Appointment date can not be past date!']" )
+    public WebElement dateTimeWarning;
+
+    @FindBy(xpath ="//button/span[text()='Send an Appointment Request']" )
+    //@FindBy(xpath ="//button[@type='submit']"]            // alternatif
+    public WebElement appointmentRequestButton;
+
+    @FindBy(xpath ="//span/strong[text()='Appointment registration saved!']" )
+    // @FindBy(xpath =" //span[text()=' We will call you as soon as possible.']")            // alternatif
+    // @FindBy(xpath ="//div/span[text()=' We will call you as soon as possible.']")
+    public WebElement appointmentSavedAlert;
+
 
 }
--- src/test/resources/features/US005.feature
+++ src/test/resources/features/US005.feature
@@ -4,18 +4,18 @@ Feature: US005 : New customers (patients) should make
 
   Scenario: Make an Appointment Testi
 
-  Given Launch web browser and navigate to the home page
-  And Click the 'Make an Appointment' button.
-  Then Verify the 'Make an Appointment' button is visible clickable.
-  Then Verify 'Make an Appointment' request form can be see.
-  And Click the 'First Name' textbox.
-  And Enter first name in the 'First Name' textbox.
-  Then Verify 'First Name' textbox is not blank.   (Your FirstName is required.)
-  Then Verify user is using characters for the 'First Name' textbox.
-  And Enter last name in the 'Last Name' textbox.
-  And Enter SSN number in the 'SSN' textbox.
-  And Enter a valid email address.
-  And Enter a valid phone number
-  And Select a valid date from 'Appointment DateTime' dropbox.
-  And Click 'Send An Appointment Request' button
-  Then Verify appointment success message which is "Appointment Registration saved. We will you call you as soon as possible."
+  Given MB Launch web browser and navigate to the home page
+  And MB Click the 'Make an Appointment' button.
+  Then MB Verify the 'Make an Appointment' button is visible clickable.
+  Then MB Verify 'Make an Appointment' request form can be see.
+  And MB Click the 'First Name' textbox.
+  And MB Enter first name in the 'First Name' textbox.
+  Then MB Verify 'First Name' textbox is not blank.   (Your FirstName is required.)
+  Then MB Verify user is using characters for the 'First Name' textbox.
+  And MB Enter last name in the 'Last Name' textbox.
+  And MB Enter SSN number in the 'SSN' textbox.
+  And MB Enter a valid email address.
+  And MB Enter a valid phone number
+  And MB Select a valid date from 'Appointment DateTime' dropbox.
+  And MB Click 'Send An Appointment Request' button
+  Then MB Verify appointment success message which is "Appointment Registration saved. We will you call you as soon as possible."
