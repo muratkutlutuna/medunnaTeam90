c79e6aa9fdfee60710ed3b8ff7484ef8a5e130a5
mehlika <mehlika.balaban@gmail.com>
2022-04-05 16:18:57 -0700

    feature ve stepdefinations calisildi.

--- src/test/java/stepDefinitions/uiStepDefinitions/firstSprint/US005.java
+++ src/test/java/stepDefinitions/uiStepDefinitions/firstSprint/US005.java
@@ -12,5 +12,163 @@ import utilities.Driver;
 public class US005 {
     MehlikaPage mehlikaPage = new MehlikaPage();
 
+    @Given("MB Launch web browser and navigate to the home page")
+    public void mb_launch_web_browser_and_navigate_to_the_home_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("medunnaUrl"));
+    }
+
+    @Then("MB Verify the Make an Appointment button is visible and clickable")
+    public void mb_verify_the_make_an_appointment_button_is_visible_and_clickable() {
+        Assert.assertTrue(mehlikaPage.makeAnAppointmentButton.isDisplayed());
+        Assert.assertTrue(mehlikaPage.makeAnAppointmentButton.isEnabled());
+    }
+
+    @Given("MB Click the Make an Appointment button")
+    public void mb_click_the_make_an_appointment_button() {
+        mehlikaPage.makeAnAppointmentButton.click();
+    }
+
+    @Then("MB Verify Make an Appointment request form can be seen")
+    public void mb_verify_make_an_appointment_request_form_can_be_seen() {
+        mehlikaPage.makeAppoinmentTitle.isDisplayed();
+    }
+
+    @Then("MB Click First Name textbox and Enter First Name {string}")
+    public void mb_click_first_name_textbox_and_enter_first_name(String firstname) {
+        mehlikaPage.firstNameInputBox.click();
+        mehlikaPage.firstNameInputBox.sendKeys(firstname + Keys.ENTER);
+    }
+
+    @Then("MB Verify First Name textbox is not blank")
+    public void mb_verify_first_name_textbox_is_not_blank() {
+        String expfirstNameWarning = "FirstName is required";
+        String actFirstNameWarning = mehlikaPage.firstNameWarning.getText();
+        Assert.assertEquals(expfirstNameWarning, actFirstNameWarning);
+        Assert.assertTrue(actFirstNameWarning.contains(expfirstNameWarning));
+    }
+
+    @Then("MB Verify user is using characters for the First Name textbox")
+    public void mb_verify_user_is_using_characters_for_the_first_name_textbox() {
+        String firstName=mehlikaPage.firstNameInputBox.getText();
+        ///////////////
+    }
+
+    @Then("MB Click the Last Name textbox and enter Last Name {string}")
+    public void mb_click_the_last_name_textbox_and_enter_last_name(String lastname) {
+        mehlikaPage.lastNameInputBox.click();
+        mehlikaPage.lastNameInputBox.sendKeys(lastname+Keys.ENTER);
+    }
+
+    @Then("MB Verify Last Name textbox is not blank")
+    public void mb_verify_last_name_textbox_is_not_blank_warning() {
+        Assert.assertFalse(mehlikaPage.lastNameInputBox.getText().isEmpty());
+        Assert.assertTrue(mehlikaPage.lastNameWarning.getText().contains("LastName is required"));
+
+    }
+
+    @Then("MB Verify user is using characters for the Last Name textbox")
+    public void mb_verify_user_is_using_characters_for_the_last_name_textbox() {
+
+    }
+
+    @Then("MB Click the SSN textbox and enter SSN number {string}")
+    public void mb_click_the_ssn_textbox_and_enter_ssn_number(String string) {
+
+    }
+
+    @Then("MB Verify SSN textbox is not blank")
+    public void mb_verify_ssn_textbox_is_not_blank() {
+
+    }
+
+    @Then("MB Verify user is using digits for SSN textbox")
+    public void mb_verify_user_is_using_digits_for_ssn_textbox() {
+
+    }
+
+    @Then("MB Verify this SSN is same as a registered SSN")
+    public void mb_verify_this_ssn_is_same_as_a_registered_ssn() {
+
+    }
+
+    @Then("MB Click Email textbox and enter a valid Email address {string}")
+    public void mb_click_email_textbox_and_enter_a_valid_email_address(String string) {
+
+    }
+
+    @Then("MB Verify Email textbox is not blank")
+    public void mb_verify_email_textbox_is_not_blank() {
+
+    }
+
+    @Then("MB Verify user uses @ and . sign")
+    public void mb_verify_user_uses_and_sign() {
+
+    }
+
+    @Then("MB Verify this number have - after 3th and 6th digits")
+    public void mb_verify_this_number_have_after_3th_and_6th_digits(Double double1) {
+
+    }
+
+    @Then("MB Click the Phone textbox and enter a valid Phone number {string}")
+    public void mb_click_the_phone_textbox_and_enter_a_valid_phone_number(String string) {
+
+    }
+
+    @Then("MB Verify that user is using digits for Phone textbox")
+    public void mb_verify_that_user_is_using_digits_for_phone_textbox() {
+
+    }
+
+    @Then("MB Verify that this Phone textbox is not blank")
+    public void mb_verify_that_this_phone_textbox_is_not_blank() {
+
+    }
+
+    @Then("MB Click the Appointment DateTime dropbox and select a valid date\"{int}\\/{int}\\/{int}\"")
+    public void mb_click_the_dropbox_and_select_a_valid_date(String string, Integer month, Integer day, Integer year) {
+
+    }
+
+    @Then("MB Verify selected date is one of following days")
+    public void mb_verify_selected_date_is_one_of_following_days() {
+
+    }
+
+    @Then("MB Click Send An Appointment Request button")
+    public void mb_click_send_an_appointment_request_button() {
+
+    }
+
+    @Then("MB Verify appointment success message which is {string}")
+    public void mb_verify_appointment_success_message_which_is(String string) {
+
+    }
+
+    @Then("MB Click Account Menu dropbox sign and click Sign In")
+    public void mb_click_account_menu_dropbox_sign_and_click_sign_in() {
+
+    }
+
+    @Then("MB Click User Name textbox and enter a valid user name {string}")
+    public void mb_click_user_name_textbox_and_enter_a_valid_user_name(String username) {
+
+    }
+
+    @Then("MB Click Password textbox and enter a valid password {string}")
+    public void mb_click_password_textbox_and_enter_a_valid_password(String password) {
+
+    }
+
+    @Then("MB Click Sign In button")
+    public void mb_click_sign_in_button() {
+
+    }
+
+    @Then("MB Verify sign in is successfully")
+    public void mb_verify_sign_in_is_successfully() {
+
+    }
 
 }
--- src/test/resources/features/US005.feature
+++ src/test/resources/features/US005.feature
@@ -4,44 +4,40 @@ Feature: US005 :  New customers (patients) should make an appoinment
   @UIMehlika
   Scenario Outline: Make an Appointment Testi
 
-    Given MB Launch web browser and navigate to the home page
-  And MB Click the Make an Appointment button.
-  Then MB Verify the Make an Appointment button is visible clickable.
-  Then MB Verify Make an Appointment request form can be seen.
-  And MB Click the First Name textbox.
-  And MB Enter "<firstname>" in the First Name textbox.
-  Then MB Verify First Name textbox is not blank.
-  Then MB Verify user is using characters for the First Name textbox.
-  And MB Click the Last Name textbox.
-  And MB Enter "<lastname>" in the Last Name textbox.
-  Then MB Verify Last Name textbox is not blank.             // (Your LastName is required.)
-  Then MB Verify user is using characters for the Last Name textbox.
-  And MB Click the SSN textbox.
-  And MB Enter "<SSN>" number in the SSN textbox.
-  Then MB Verify SSN textbox is not blank.                    // (Your SSN is required.)
-  Then MB Verify user is using numbers for SSN textbox.
-  Then MB Verify this SSN is.same as a registered SSN
-  And MB Click  Email textbox.
-  And MB Enter a valid "<email>" address.
-  Then MB Verify Email textbox is not blank.                  // (Your Email is required.)
-  Then MB Verify user uses @ and . sign.
-  Then MB Verify this number have - after 3th and 6th digits.
-  And MB Click the Phone textbox.
-  And MB Enter a valid "<phone>"
-  Then MB Verify that user is using digits for Phone textbox.
-  Then MB Verify that this Phone textbox is not blank.
-  And MB Select valid "<date>" from Appointment DateTime dropbox.
-  Then MB Verify selected date is one of following days.
+  Given MB Launch web browser and navigate to the home page
+  Then MB Verify the Make an Appointment button is visible and clickable
+  And MB Click the Make an Appointment button
+  Then MB Verify Make an Appointment request form can be seen
+  And MB Click First Name textbox and Enter First Name "<firstname>"
+  Then MB Verify First Name textbox is not blank
+  Then MB Verify user is using characters for the First Name textbox
+  And MB Click the Last Name textbox and enter Last Name "<lastname>"
+  Then MB Verify Last Name textbox is not blank
+  Then MB Verify user is using characters for the Last Name textbox
+  And MB Click the SSN textbox and enter SSN number "<SSN>"
+  Then MB Verify SSN textbox is not blank
+  Then MB Verify user is using digits for SSN textbox
+  Then MB Verify this SSN is same as a registered SSN
+  And MB Click Email textbox and enter a valid Email address "<email>"
+  Then MB Verify Email textbox is not blank
+  Then MB Verify user uses @ and . sign
+  Then MB Verify this number have - after 3th and 6th digits
+  And MB Click the Phone textbox and enter a valid Phone number "<phone>"
+  Then MB Verify that user is using digits for Phone textbox
+  Then MB Verify that this Phone textbox is not blank
+  And MB Click the Appointment DateTime dropbox and select a valid date"<date>"
+  Then MB Verify selected date is one of following days
   And MB Click Send An Appointment Request button
   Then MB Verify appointment success message which is "Appointment Registration saved. We will you call you as soon as possible."
-  And MB Click Account Menu sign
-  And MB Click Sign In text
-  And MB Click User Name textbox
-  And MB Enter a valid "<username>"
-  And MB Click Password textbox
-  And MB Enter a valid "<password>"
+  And MB Click Account Menu dropbox sign and click Sign In
+  And MB Click User Name textbox and enter a valid user name "<username>"
+  And MB Click Password textbox and enter a valid password "<password>"
   And MB Click Sign In button
-  Then MB Verify sign in is successfully.
+  Then MB Verify sign in is successfully
+
+
+
+
 
     Examples: test data
       |firstname|lastname|  SSN   |      email         |phone       |date     |username|password|
