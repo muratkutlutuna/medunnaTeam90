7dce4163a9346195c162f2c0fc5684b9343675ba
Diana Turkmen <cemile.turkmen@gmail.com>
2022-04-06 23:41:10 +0100

    first commit

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -14,8 +14,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = {"stepDefinitions","hooks"},
-        tags = "@UIRegistration",
-        dryRun = true
+        tags = "@rememberMe",
+        dryRun = false
 )
 public class Runner {
 }
