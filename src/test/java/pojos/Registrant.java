745fa67e06a9c5d840c0b0641ccf195b466ad1b0
mehlika <mehlika.balaban@gmail.com>
2022-04-11 11:51:20 -0700

    son

--- src/test/java/pojos/Registrant.java
+++ src/test/java/pojos/Registrant.java
@@ -1,14 +1,14 @@
 package pojos;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@JsonIgnoreProperties(ignoreUnknown = true)
 
 public class Registrant {
 
