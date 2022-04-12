85c1b153079f0f493d582f1103ceb2a64a11d1e3
Rana <ranaterzioglu@gmail.com>
2022-04-12 10:41:18 -0400

    Us002 Api test finished

--- src/test/java/utilities/Authentication.java
+++ src/test/java/utilities/Authentication.java
@@ -11,7 +11,7 @@ import static io.restassured.RestAssured.given;
 
 public class Authentication {
     public static void main(String[] args) {
-        String guncelToken = generateToken();
+        String guncelToken = generateToken("kurt890","kurt890");
         System.out.println(guncelToken);
     }
 
@@ -19,6 +19,21 @@ public class Authentication {
     public static String generateToken(){
         String username = "Team90Admin";
         String password = "Batch44+";
+
+        Map<String,Object>map = new HashMap<String,Object>();
+        map.put("username", username);
+        map.put("password", password);
+        map.put("rememberme", true);
+
+        String endPoint = "https://www.medunna.com/api/authenticate";
+
+        Response response = given().contentType(ContentType.JSON).body(map).when().post(endPoint);
+
+        JsonPath token = response.jsonPath();
+
+        return token.getString("id_token");
+    }
+    public static String generateToken(String username,String password){
 
         Map<String,Object>map = new HashMap<String,Object>();
         map.put("username", username);
