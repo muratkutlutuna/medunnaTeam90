7c7edb6aa57b533114c35f66cb9fc1150c556722
Murat_Kutlu_Tuna <muratkutlutuna@gmail.com>
2022-04-13 16:07:23 +0200

    degisiklik

--- src/test/java/stepDefinitions/apiStepDefinitions/US001.java
+++ src/test/java/stepDefinitions/apiStepDefinitions/US001.java
@@ -37,6 +37,7 @@ public class US001 {
         registrant.setPassword(SSN+firstname);
         registrant.setEmail(faker.internet().emailAddress(firstname+lastname));
         response = given().spec(spec)
+                .header("Authentication","Bearer "+generateToken())
                 .contentType(ContentType.JSON)
                 .body(registrant)
                 .when()
